839c259e9def04f6a81f57f2bce431365ac14b3c
Jérémy Bojko <jeremy.bojko@protonmail.com>
2022-06-21 11:12:59 +0200

    Route Avancées - Custom PreloadStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
-import { NoPreloading, PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomPreloadStrategyService } from './core/services/custom-preload-strategy.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' }, // localhost:4200
-  { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)},
-  { path: 'clients', loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule) },
-  { path: '**', loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)}
+  { 
+    path: 'orders', 
+    loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule), 
+    data : { preload: true}
+  },
+  { 
+    path: 'clients', 
+    loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule) 
+  },
+  { 
+    path: '**', 
+    loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule), 
+    data : { preload: true}
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, 
-    { preloadingStrategy: PreloadAllModules })],
+    { preloadingStrategy: CustomPreloadStrategyService })],
+    providers: [CustomPreloadStrategyService],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
